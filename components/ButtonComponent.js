f6a9446f079d6b2bf0b4fbc62cc2df4bd85ac8df
MossaffaMughal <mossaffa.msm@gmail.com>
2024-12-08 18:12:44 +0500

    Completed the Home Screen and Basic Navigation to Allergen Check and Boycott Check Screen

---
+++ components/ButtonComponent.js
@@ -0,0 +1,49 @@
+// src/components/ButtonComponent.js
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const ButtonComponent = ({ title, onPress }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 15,
+    backgroundColor: "white",
+    borderColor: "#008000",
+    borderWidth: 4,
+    borderRadius: 20,
+    marginBottom: 15,
+    alignItems: "center",
+    width: 200,
+    // Shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    // Elevation for Android
+    elevation: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    backgroundColor: "white",
+    borderRadius: 20,
+    marginBottom: 15,
+    alignItems: "center",
+    width: 200,
+    borderWidth: 4,
+    borderColor: "#0d640d",
+  },
+  buttonText: {
+    color: "#0d640d",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
+
+export default ButtonComponent;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,129 @@
+// src/screens/HomeScreen.js
+import React from "react";
+import { View, StyleSheet, Image, Text } from "react-native";
+import ButtonComponent from "../components/ButtonComponent";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      {/* Background Image - health*/}
+      <Image
+        source={require("../assets/images/background_health.png")}
+        style={styles.backgroundImagehealth}
+      />
+      {/* Logo */}
+      <Image
+        source={require("../assets/images/logo.png")}
+        style={styles.logo}
+      />
+
+      {/* Buttons */}
+      <ButtonComponent
+        title="Allergen Check"
+        onPress={() => navigation.navigate("AllergenCheck")}
+        style={styles.button}
+      />
+      <ButtonComponent
+        title="Boycott Check"
+        onPress={() => navigation.navigate("BoycottCheck")}
+        style={styles.button}
+      />
+
+      {/* Footer Section */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require("../assets/images/home_icon.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.iconText}>Home</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require("../assets/images/about_icon.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.iconText}>About</Text>
+        </View>
+      </View>
+
+      {/* Background Image - boycott */}
+      <Image
+        source={require("../assets/images/background_boycott.png")}
+        style={styles.backgroundImageboycott}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "green",
+    marginBottom: 30,
+    backgroundColor: "transparent", // Ensuring the background image shows through
+  },
+  backgroundImagehealth: {
+    position: "absolute",
+    bottom: 300,
+    width: 650,
+    height: 650,
+    opacity: 0.15,
+    zIndex: 1,
+  },
+  backgroundImageboycott: {
+    position: "absolute",
+    top: 500,
+    left: 0.1,
+    width: 650,
+    height: 650,
+    opacity: 0.7,
+    zIndex: -1,
+    transform: [{ rotate: "-25deg" }, { scale: 1 }],
+  },
+  logo: {
+    width: 400,
+    height: 400,
+    zIndex: 1,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    bottom: 30,
+    width: "80%", // Adjust the width to your preference
+    paddingHorizontal: 10,
+    zIndex: 1,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginBottom: 10,
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconText: {
+    color: "#0d640d",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  button: {
+    zIndex: 1,
+  },
+});
+
+export default HomeScreen;
